696ee46819a4a3cf2097c5fc6eb4f8c7eded43b5
Vlad Filip <vfilip@inthergroup.com>
2022-12-20 09:00:42 +0200

    2014

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -96,4 +96,65 @@ Bmw.accelerate();
 
 Mercedes.accelerate();
 Mercedes.decelerate();
+
+
+//Class expresion
+// const PersonCl = class {};
+
+// Class declaration
+class PersonCl {
+  constructor(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  }
+  // Methods will be added to .prototype property
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  }
+  greet() {
+    console.log(`Hey ${this.firstName}`);
+  }
+  get age() {
+    return 2037 - this.birthYear;
+  }
+  // Set a property that already exists
+  set fullName(name) {
+    console.log(name);
+    if (name.includes(' ')) this._fullNAme = name;
+    else alert(`${name} is not a full name!`);
+  }
+  get fullName() {
+    return this._fullNAme;
+  }
+}
+
+const jessica = new PersonCl('Jessica', 1996);
+console.log(jessica);
+jessica.calcAge();
+console.log(jessica.age);
+console.log(jessica.__proto__ === PersonCl.prototype);
+
+// PersonCl.prototype.greet = function () {
+//   console.log(`Hey ${this.firstName}`);
+// };
+jessica.greet();
+
+// 1. Classes NOT hoisted
+// 2.Class are first-class citizes
+// 3. Classes are executed in stric mode
+const walter = new PersonCl('Walter', 1965);
+const account = {
+  owner: 'Vlad',
+  movements: [200, 530, 123, 343],
+
+  get latest() {
+    return this.movements.slice(-1).pop();
+  },
+  set latest(mov) {
+    this.movements.push(mov);
+  },
+};
+console.log(account.latest);
+account.latest = 50;
+console.log(account.movements);
 */
